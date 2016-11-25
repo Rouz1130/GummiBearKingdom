9285c6bb97cc9a3f5032646600a3abf1e657b7ed
rouz majlessi <rmajlessi@gmail.com>
2016-11-24 17:15:13 -0800

    add products and blog controllers

---
+++ src/GummiBearKingdom/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GummiBearKingdom.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GummiBearKingdomContext db = new GummiBearKingdomContext();
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            var this.Product = db.Products.FirstOrDefault(ProductsController => ProductsController.ProductsId == id);
+            return View(thisProduct);
+        }
+    }
+}
